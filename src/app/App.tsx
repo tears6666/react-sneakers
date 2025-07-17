8b5e2fe4085125cf4761196d1696b3181d19e362
Alexey Chernov <leshachernov200@mail.ru>
2025-07-17 13:17:51 +1000

    init project

---
+++ src/app/App.tsx
@@ -0,0 +1,6 @@
+import Header from '../components/Header/Header';
+export default function App(){
+  return(
+    <Header />
+  )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { IoMdBasket } from "react-icons/io";
+import { MdFavoriteBorder } from "react-icons/md";
+import Logo from '../../img/logo.jpeg';
+import styles from './Header.module.scss';
+
+export default function Header(){
+  return(
+    <header className={styles.header}>
+      
+      <div className={styles.header__launch}>
+        <img src={Logo} alt='logo' width={70}/>
+        <div className={styles.launch__aside}>
+          <h1 className={styles.aside__logo}>React-Sneakers</h1>
+          <p className={styles.aside__text}>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <nav className={styles.header__nav}>
+        <ul className={styles.nav__list}>
+          <IoMdBasket className={styles.list__basket}/>
+          <MdFavoriteBorder className={styles.list__fav}/>
+        </ul>
+      </nav>
+    </header>
+  )
+}
